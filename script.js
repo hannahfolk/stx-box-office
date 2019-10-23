8110f073d6921892f16f38091e33b511698a1c55
hfolk25 <hfolk25@gmail.com>
2019-10-22 22:47:33 -0700

    updating site

--- script.js
+++ script.js
@@ -1,10 +1,72 @@
 $(document).ready(function() {
-    var queryURL = "https://app.ticketmaster.com/discovery/v1/events.json?apikey=stYbjS0YJC8P0IGe0x2ihjPsWAQpASOr&callback=myFunction";
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    });
+
+    function writeForm(event) {
+        event.preventDefault();
+
+        var divEl = $("#divEl");
+        $("#input-value").empty();
+        divEl.empty();
+        var divElLabel = $("<label>");
+        var formInput = $("<input>");
+        var formSubmit = $("<button>");
+
+        divEl.attr("class", "form-group col-md-6");
+        divElLabel.attr("for", "searchBar");
+        formInput.attr("type", "text");
+        formInput.attr("class", "form-control");
+        formInput.attr("id", "form-input");
+        formSubmit.attr("type", "submit");
+
+        divElLabel.text("Please enter " + $(this).val() + ":");
+        formSubmit.text("Submit");
+
+        $("#input-value").append(divEl);
+        divEl.append(divElLabel);
+        divEl.append(formInput);
+        $("#input-value").append(formSubmit);
+
+        formSubmit.on("click", function(event) {
+            event.preventDefault();
+            console.log(formInput.val());
+
+            formInput.val("");
+        });
+    };
+
+    $(document).on("click", ".dropdown-item", writeForm);
+    // ==================REVENUE DATA================= //
+    // Create variables
+
+    // if ($(this).val() === "movie title" || $(this).val() === "Zip Code" || $(this).val() === "actor name" || $(this).val() === "Directors") {
+    // };
+    // If genre is selected, return all movie titles.
+    // Make a looped AJAX call to OPUS and return revenue data.
+
+    // ==================GENRE======================== //
+
+    // ==================YEAR========================= //
+
+    // ==================YEAR RANGE=================== //
+
+    // ==================ACTORS======================= //
+
+    // ==================THEATER CHAINS=============== //
+
+    // ==================RATED======================== //
+
+    // ==================RATINGS====================== //
+
+    // ==================BOX OFFICE HARD DOLLARS====== //
+        // OPUS
+
+    // ==================BOX OFFICE RANGE============= //
+        // OPUS
+
+    // ==================DMA========================== //
+
+    // ==================SCREEN COUNT================= //
+        // OPUS
+
+    // ==================ZIP CODE===================== //
+
 });
