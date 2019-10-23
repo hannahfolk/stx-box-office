0779e634f57d47cbbdb82f958a72555799589da6
hfolk25 <hfolk25@gmail.com>
2019-10-22 23:20:21 -0700

    updating site

--- script.js
+++ script.js
@@ -33,6 +33,14 @@ $(document).ready(function() {
         });
     };
 
+    $(function() {
+        $('input[name="daterange"]').daterangepicker({
+          opens: 'left'
+        }, function(start, end, label) {
+          console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+        });
+      });
+
     $(document).on("click", ".dropdown-item", writeForm);
     // ==================REVENUE DATA================= //
     // Create variables
