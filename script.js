ae9715a311be454dfd159f85bc5f54341e829f04
hfolk25 <hfolk25@gmail.com>
2019-10-27 11:26:58 -0700

    saving work

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
         }, function(start, end, label) {
           console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
         });
-      });
+    });
 
     $(document).on("click", ".dropdown-item", writeForm);
     // ==================REVENUE DATA================= //
@@ -76,5 +76,66 @@ $(document).ready(function() {
         // OPUS
 
     // ==================ZIP CODE===================== //
+
+// var data;
+// $.ajax({
+//   type: "GET",
+//   url: "js-tutorials.com_sample_file.csv",
+//   dataType: "text",  
+//   success: function(response)  
+//   {
+//     data = $.csv.toArrays(response);
+//     generateHtmlTable(data);
+//   }   
+// });
+
+// function generateHtmlTable(data) {
+//     var html = '<table  class="table table-condensed table-hover table-striped">';
+
+//       if(typeof(data[0]) === 'undefined') {
+//         return null;
+//       } else {
+// 		$.each(data, function( index, row ) {
+// 		  //bind header
+// 		  if(index == 0) {
+// 			html += '<thead>';
+// 			html += '<tr>';
+// 			$.each(row, function( index, colData ) {
+// 				html += '<th>';
+// 				html += colData;
+// 				html += '</th>';
+// 			});
+// 			html += '</tr>';
+// 			html += '</thead>';
+// 			html += '<tbody>';
+// 		  } else {
+// 			html += '<tr>';
+// 			$.each(row, function( index, colData ) {
+// 				html += '<td>';
+// 				html += colData;
+// 				html += '</td>';
+// 			});
+// 			html += '</tr>';
+// 		  }
+// 		});
+// 		html += '</tbody>';
+// 		html += '</table>';
+// 		alert(html);
+// 		$('#csv-display').append(html);
+// 	  }
+//     }
+
+    var proxyURL = 'https://cors-anywhere.herokuapp.com/';
+    var targetURL = 'https://access.opusdata.com/session/create?email=hfolk25@gmail.com&password=opusdatat43';
+
+    $.ajax({
+        url: proxyURL + targetURL,
+        method: "GET"
+    })
+    .then(response => JSON.parse(response))
+    .then(function(response) {
+        console.log(response);
+        console.log(response.session_id);
+    });
 
 });
